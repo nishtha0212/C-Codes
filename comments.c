4abe04e1febc5405576c2f39979c2ce56d0190df
nishtha0212 <64403766+nishtha0212@users.noreply.github.com>
2020-06-25 19:32:28 +0530

    Add files via upload

---
+++ comments.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Display use of comments.\n");
+
+    //this is a single line comment
+    printf("Comments are not displayed in output\n");
+
+    /*This
+    is a
+    multi line
+    comment*/
+}
---
+++ escape_seq.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Escape Sequence");
+
+    printf("\nThis is in new line");
+
+    printf("\nHere is tab \t space");
+
+    printf("\nSpecial symbol : \\ \a");
+}
